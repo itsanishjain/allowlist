72f53bc25df12583c80abc6dc90855272eecb703
ayushgupta0010 <ayushgupta0010@gmail.com>
2022-04-29 17:07:39 +0530

    Add firebase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
+import React, { useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../src/utils/firebase";
+
 const Home = () => {
+  useEffect(() => {
+    getDocs(collection(db, "test")).then((snapshot) =>
+      snapshot.docs.map((doc) => console.log(doc.data()))
+    );
+  }, []);
+
   return <div>Home Page</div>;
 };
 
