1da6872c16b963c4436bc3d92dc98c8793df854f
anish jain <helloanishjain@gmail.com>
2022-05-14 18:12:44 +0530

    nav and loading component added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-import Footer from "./Footer";
+// import Footer from "./Footer";
 import Navigation from "./Navigation";
 
 const Layout = ({ children }) => (
   <div>
     <Navigation />
     {children}
-    <Footer />
+    {/* <Footer /> */}
   </div>
 );
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,31 +1,126 @@
-import React from "react";
+import { useState, useEffect, useRef } from 'react'
 import Link from "next/link";
 
-const Navigation = () => (
-  <div className='space-x-4'>
-    <Link href='/'>
-      <a>name not decied yet</a>
-    </Link>
+const Navigation = () => {
+  const [toggle, setToggle] = useState(false)
+  const navRef = useRef()
 
-    <Link href='/login'>
-      <a>Login</a>
-    </Link>
+  useEffect(() => {
 
-    <Link href='/protected'>
-      <a>Protected</a>
-    </Link>
+    const body = document.body
 
-    <Link href='/dashboard/new'>
-      <a>Create Project</a>
-    </Link>
+    // Disable scrolling
+    const customBodyStyle = ["overflow-hidden", "lg:overflow-visible"]
+    if (toggle) body.classList.add(...customBodyStyle)
+    // Enable scrolling
+    else body.classList.remove(...customBodyStyle)
 
-    <Link href='/dashboard/'>
-      <a>Dashboard</a>
-    </Link>
+    // Sticky strick
+    const customStyle = ["sticky-nav", "fixed", "border-b"]
+    window.onscroll = () => {
+      if (window.scrollY > 80) navRef.current.classList.add(...customStyle)
+      else navRef.current.classList.remove(...customStyle)
+    }
+  }, [toggle])
 
-    <Link href='/users/2S5k5ROPPfzagQ7NnBuk'>Public link</Link>
-    <Link href='/dashboard/2S5k5ROPPfzagQ7NnBuk/settings'>Update project</Link>
-  </div>
-);
+
+  const navigation = [
+    { title: "Protected", path: "/protected" },
+    { title: "Dashboard", path: "/dashboard" },
+    { title: "Create project", path: "/dashboard/new" },
+  ]
+
+
+  return (
+    // <div className='space-x-4'>
+    //   <Link href='/'>
+    //     <a>name not decied yet</a>
+    //   </Link>
+
+    //   <Link href='/login'>
+    //     <a>Login</a>
+    //   </Link>
+
+    //   <Link href='/protected'>
+    //     <a>Protected</a>
+    //   </Link>
+
+    //   <Link href='/dashboard/new'>
+    //     <a>Create Project</a>
+    //   </Link>
+
+    //   <Link href='/dashboard/'>
+    //     <a>Dashboard</a>
+    //   </Link>
+
+    //   <Link href='/users/2S5k5ROPPfzagQ7NnBuk'>Public link</Link>
+    //   <Link href='/dashboard/2S5k5ROPPfzagQ7NnBuk/settings'>Update project</Link>
+    // </div>
+
+    <nav ref={navRef} className="bg-white w-full top-0 z-20">
+      <div className="items-center px-4 max-w-screen-xl mx-auto lg:flex lg:px-8">
+        <div className="flex items-center justify-between py-3 lg:py-4 lg:block">
+          <Link href="/">
+            <a className='text-xl'>Home</a>
+          </Link>
+          <div className="lg:hidden">
+            <button className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
+              onClick={() => setToggle(prev => !prev)}
+            >
+              {
+                toggle ? (
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
+                  </svg>
+                ) : (
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 8h16M4 16h16" />
+                  </svg>
+                )
+              }
+            </button>
+          </div>
+        </div>
+        <div className={`flex-1 justify-between flex-row-reverse lg:overflow-visible lg:flex lg:pb-0 lg:pr-0 lg:h-auto ${toggle ? 'h-screen pb-20 overflow-auto pr-4' : 'hidden'}`}>
+          <div>
+            <ul className="flex flex-col-reverse space-x-0 lg:space-x-6 lg:flex-row">
+
+              <li className="mt-8 lg:mt-0 mb-8 lg:mb-0">
+                <Link href="/login">
+                  <a className="py-3 px-4 text-center text-white bg-indigo-600 hover:bg-indigo-700 rounded-md shadow block lg:inline"
+                    onClick={() => setToggle(prev => !prev)}>
+                    Login
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="flex-1">
+            <ul className="justify-center items-center space-y-8 lg:flex lg:space-x-6 lg:space-y-0">
+              {
+                navigation.map((item, idx) => {
+                  return (
+                    <li key={idx} className="text-gray-600 hover:text-indigo-600">
+                      <Link href={item.path}>
+                        <a onClick={() => setToggle(prev => !prev)}>
+                          {item.title}
+                        </a>
+                      </Link>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+
+
+
+
+  )
+};
 
 export default Navigation;
