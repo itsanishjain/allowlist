24c643028cc06ba5df892bf654fc3fc5d24fc815
ayushgupta0010 <ayushgupta0010@gmail.com>
2022-05-20 19:20:35 +0530

    Fix Mint page

--- pages/mint.js
+++ pages/mint.js
@@ -1,10 +1,55 @@
-import React from "react";
-import Mint from "../src/components/Mint";
+import { useContext, useState } from "react";
+import { providers, Contract, utils } from "ethers";
 
-const MintPage = () => (
-  <div>
-    <Mint />
-  </div>
-);
+import { UserContext } from "../src/context/UserContext";
+import { allowlistABI } from "../src/smartContract/allowlistABI";
+import { INFURA_URL } from "../src/utils/constants";
+
+const ALLOWLIST_CONTRACT = "0xfdb45a71fa1761fb43d2d665a3e1cc4a31b10e4c";
+
+const MintPage = () => {
+  const { library, chainId } = useContext(UserContext);
+
+  const [minting, setMinting] = useState(false);
+
+  const mintNFT = async () => {
+    setMinting(true);
+
+    try {
+      if (chainId == 4 && library.connection.url != "metamask") {
+        library.provider.http.connection.url = INFURA_URL;
+      }
+
+      const provider = await library.provider;
+      const web3Provider = new providers.Web3Provider(provider);
+
+      const contract = new Contract(
+        ALLOWLIST_CONTRACT,
+        allowlistABI,
+        web3Provider.getSigner()
+      );
+
+      const tx = await contract.mint(1, {
+        value: utils.parseEther("0"),
+      });
+
+      await tx.wait();
+      setMinting(false);
+
+      alert("You Successfully Minted an NFT");
+    } catch (err) {
+      console.log(err);
+      alert("Something went wrong");
+    }
+  };
+
+  return (
+    <div>
+      {!minting ? <button onClick={mintNFT}>Mint</button> : "Minting......."}
+
+      {library ? "YES" : "Noooooooo"}
+    </div>
+  );
+};
 
 export default MintPage;
---
+++ src/utils/constants.js
@@ -0,0 +1,1 @@
+export const INFURA_URL = `https://rinkeby.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_KEY}`;
