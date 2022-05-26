3ea6f5d8f62f0634ce4f5fe1c83de1cf384c9ca1
ayushgupta0010 <ayushgupta0010@gmail.com>
2022-05-26 19:45:27 +0530

    Fix random winners generator

--- src/components/RegisterUserList.jsx
+++ src/components/RegisterUserList.jsx
@@ -1,37 +1,26 @@
 import { useState } from "react";
-import toast from "react-hot-toast";
-import Input from '../components/Input'
+import Input from "../components/Input";
 
 const RegisterUserList = ({ users }) => {
   const [winners, setWinners] = useState([]);
   const [winnersCount, setWinnersCount] = useState(1);
 
   const selectRandomWinners = () => {
-    if (winnersCount > users.length) {
-      toast.error('Exceed')
-      return;
-    }
-    if (winnersCount === users.length) {
-      setWinners(users);
-      return;
-    }
-    let tempWinners = []
-    while (true) {
-      const randomNumber = Math.floor(Math.random() * users.length)
-      if (!tempWinners.includes(randomNumber)) {
-        tempWinners.push(randomNumber)
-      }
-      if (tempWinners.length === winnersCount) break;
-    }
-    setWinners(tempWinners.map((w) => users[w]));
-  }
-
-
-  console.log(winners);
+    let randomNumbers = [...Array(users.length).keys()];
+    randomNumbers.sort(() => Math.random() - 0.5);
+    setWinners(randomNumbers.slice(0, winnersCount).map((i) => users[i]));
+  };
 
   return (
     <div className='max-w-xl mx-auto'>
-      <Input type="number" inputTagType="smallInput" name="winnerCount" placeholder="winner count" value={winnersCount} onChange={(e) => setWinnersCount(e.target.value)} />
+      <Input
+        type='number'
+        inputTagType='smallInput'
+        name='winnerCount'
+        placeholder='winner count'
+        value={winnersCount}
+        onChange={(e) => setWinnersCount(e.target.value)}
+      />
       <button onClick={selectRandomWinners}>Select Random Winners</button>
       <button>Download CSV</button>
       <div className='bg-gray-100 shadow-sm p-8 rounded-md space-y-4 font-mono'>
@@ -44,15 +33,15 @@ const RegisterUserList = ({ users }) => {
           </div>
         ))}
       </div>
-      <div className="mt-8">
-        {
-          winners.map((winner, index) => (
-            <p key={index}>{users[winner]}</p>
-          ))
-        }
+      <div className='mt-8'>
+        {winners.map((winner, i) => (
+          <p className='text-red-500' key={i}>
+            {winner}
+          </p>
+        ))}
       </div>
     </div>
-  )
+  );
 };
 
 export default RegisterUserList;
