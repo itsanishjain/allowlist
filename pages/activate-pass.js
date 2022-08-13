695ac1531d60b8a881556429eff9dc79fc2ce21d
anishjain <helloanishjain@gmail.com>
2022-08-13 20:18:03 +0530

    add cronos mainnettitle for minting

--- pages/activate-pass.js
+++ pages/activate-pass.js
@@ -17,19 +17,19 @@ const ActivatePass = () => {
   }, [isLoggedIn, router]);
 
   return (
-    <div className='mt-8 max-w-3xl mx-auto flex flex-col items-center space-y-4'>
-      <p className='text-xl text-center'>Activate Pass</p>
-      <div className='w-full md:w-1/2 p-1'>
-        <div className='w-full h-auto overflow-hidden object-cover'>
-          <Image src={allowlistNFTImage} alt='hero image' />
+    <div className="mt-8 max-w-3xl mx-auto flex flex-col items-center space-y-4">
+      <p className="text-xl text-center">Activate Pass</p>
+      <div className="w-full md:w-1/2 p-1">
+        <div className="w-full h-auto overflow-hidden object-cover">
+          <Image src={allowlistNFTImage} alt="hero image" />
         </div>
       </div>
       {allowlistNFT.isActivated ? (
-        <p className='font-mono text-xl text-green-400'>Activated</p>
+        <p className="font-mono text-xl text-green-400">Activated</p>
       ) : !allowlistNFT.isChainIdWrong ? (
         <Mint />
       ) : (
-        <p>Please connect to Polygon Mainnet</p>
+        <p>Please connect to Cronos Mainnet [Beta]</p>
       )}
     </div>
   );
